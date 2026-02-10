e95004b89dbb37efc16c2478f724991edd00d982
Dhruv Patni <patnidhruv13@gmail.com>
2026-02-10 15:06:48 +0530

    Updated api index method to add filters.

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuctionController;
 
-// Get all auctions
-Route::get('/auctions', [AuctionController::class, 'index']);
-
-// Get single auction
-Route::get('/auctions/{id}', [AuctionController::class, 'show']);
+Route::apiResource('auctions', AuctionController::class);
